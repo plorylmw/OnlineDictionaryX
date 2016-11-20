595d800a86e1b8a3fff39fb0c8e99fb64ea11159
limingwei <936961011@qq.com>
2016-11-20 19:43:40 +0800

    add server_client

---
+++ src/serverclient/MutiThreadServer.java
@@ -0,0 +1,118 @@
+package serverclient;
+
+import translator.bing.BingTranslate;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Created by limingwei on 16/11/20.
+ */
+
+public class MutiThreadServer extends JFrame//多线程服务器
+{
+    private JTextArea jta = new JTextArea();
+
+    public static void main(String[] args)
+    {
+        new MutiThreadServer();
+    }
+
+    public MutiThreadServer()
+    {
+        setLayout(new BorderLayout());
+        add(new JScrollPane(jta), BorderLayout.CENTER);
+
+        setTitle("MutiThreadServer");
+        setSize(500, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(8000);//建立端口号为8000的套接字
+            jta.append("MutiThreadServer started at " + new Date() + '\n');
+
+            int clientNo = 1;
+
+            while(true)
+            {
+                Socket socket = serverSocket.accept();
+                jta.append("Start thread for client " + clientNo + " at " + new Date() + '\n');
+
+                InetAddress inetAddress = socket.getInetAddress();
+                jta.append("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + "\n");
+                jta.append("Client " + clientNo + "'s IP Address is " + inetAddress.getHostAddress() + "\n");
+
+                HandleAClient task = new HandleAClient(socket);
+
+                new Thread(task).start();//开辟一个新的线程
+
+                clientNo++;
+            }
+        }
+        catch(IOException ex)
+        {
+            System.err.println(ex);
+        }
+    }
+
+    class HandleAClient implements Runnable
+    {
+        private Socket socket;
+
+        public HandleAClient(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        public void run()
+        {
+            try
+            {
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while(true)
+                {
+                    String words = inputFromClient.readUTF();
+                    String bingTranslateResult = bingTranslate(words);
+
+                    outputToClient.writeUTF(bingTranslateResult);
+
+                    jta.append("Words received frome client: " + words + '\n');
+                    jta.append("explaination found: " + bingTranslateResult + '\n');
+                }
+            }
+            catch(IOException e)
+            {
+                System.err.println(e);
+            }
+        }
+
+        public String bingTranslate(String words)
+        {
+            StringBuilder result = new StringBuilder();
+            result.append("Bing释义:" + '\n');
+
+            BingTranslate bingTranslate = new BingTranslate();
+
+            try {
+                String youdaoResult = bingTranslate.translate(words);
+                result.append(youdaoResult);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result.toString();
+        }
+
+    }
+}
--- src/translator/bing/BingTranslate.java
+++ src/translator/bing/BingTranslate.java
@@ -17,12 +17,12 @@ import java.util.regex.Pattern;
 
 public class BingTranslate
 {
-    public static void main(String[] args) throws IOException {
+    public String translate(String word) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        System.out.print("请输入你要查的单词:");
-        Scanner s = new Scanner(System.in);
-        String word = s.nextLine();
+        //System.out.print("请输入你要查的单词:");
+        //Scanner s = new Scanner(System.in);
+        //String word = s.nextLine();
         word = word.replaceAll(" ","+");
 
         //根据查找单词构造查找地址
@@ -34,19 +34,28 @@ public class BingTranslate
         //注意(?s)，意思是让'.'匹配换行符，默认情况下不匹配
         Pattern searchMeanPattern = Pattern.compile("<meta name=\"description\" content=(.*?) />");
         Matcher m1 = searchMeanPattern.matcher(result);
-        if (m1.find()) {
+        if (m1.find())
+        {
             String means = m1.group();//所有解释，包含网页标签
             String[] m2=means.split("，");
             System.out.println("释义:");
-            if(m2.length>=3) {
+            if(m2.length>=3)
+            {
+                StringBuilder str = new StringBuilder();
                 String[] p=m2[3].split("\"");
                 String[] m=p[0].split(" ");
                 for(int i=0;i*2<m.length;i++)
-                    System.out.println("\t"+m[i*2]+" "+m[i*2+1]);
+                    str.append("\t"+m[i*2]+" "+m[i*2+1] + "\n");
+                return str.toString();
             }
-        } else {
-            System.out.println("未查找到释义.");
-            System.exit(0);
+            else
+            {
+                return "未找到释义.";
+            }
+        }
+        else
+        {
+            return "未找到释义.";
         }
     }
 }
